10c6ca35e883ae4633b5f364efdaa919b6ffe877
hungson100204 <hungson100204@gmail.com>
2025-05-17 10:48:20 +0700

    update dashboard

--- api/models/Sequelize-mysql.js
+++ api/models/Sequelize-mysql.js
@@ -16,6 +16,7 @@ const sequelize = new Sequelize({
   username: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "151004abyss",
   password: process.env.DB_PASSWORD || "453145",
+  password: process.env.DB_PASSWORD || "10022004",
   database: process.env.DB_NAME || "fiverr_new",
   port: 3306,
 });
